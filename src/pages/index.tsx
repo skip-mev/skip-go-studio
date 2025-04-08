68d3b5acbad2d006a59066a305078b2eaece7a74
Nur Fikri <codingki@gmail.com>
2025-04-08 07:01:13 +0700

    fix layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,74 +22,66 @@ export default function Studio() {
         backgroundColor: backgroundColor,
       }}
     >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
-        <div className="mx-auto w-7xl flex h-screen">
-          {/* Content goes here */}
-          {/* Left Sidebar */}
-          <div className="flex w-[472px] flex-col justify-between px-6 text-white relative">
-            <div className="flex flex-col gap-2.5 overflow-y-scroll">
-              <Card>
-                <h1 className="text-2xl">Get started with skip:go</h1>
-                <p>
-                  Onboard users from anywhere to your app with Skip:Go. Choose a
-                  starting template, or configure the widget to suit your needs.
-                </p>
-              </Card>
-              <Card>
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-2xl">Filter</h1>
-                  <p>
-                    Select the chains and assets you want to support in the
-                    widget
-                  </p>
-                </div>
-                <ChainAndAssetFilter context="source" />
-                <ChainAndAssetFilter context="destination" />
-              </Card>
-              <Card>
-                <h1 className="text-2xl">Styling</h1>
-                <ChainAndAssetFilter context="source" />
-                <ChainAndAssetFilter context="destination" />
-              </Card>
+      <div className="flex w-[472px] flex-col justify-between text-white relative">
+        <div className="flex flex-col gap-2.5 overflow-y-scroll p-6">
+          <Card>
+            <h1 className="text-2xl">Get started with skip:go</h1>
+            <p>
+              Onboard users from anywhere to your app with Skip:Go. Choose a
+              starting template, or configure the widget to suit your needs.
+            </p>
+          </Card>
+          <Card>
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl">Filter</h1>
+              <p>
+                Select the chains and assets you want to support in the widget
+              </p>
             </div>
+            <ChainAndAssetFilter context="source" />
+            <ChainAndAssetFilter context="destination" />
+          </Card>
+          <Card>
+            <h1 className="text-2xl">Styling</h1>
+            <ChainAndAssetFilter context="source" />
+            <ChainAndAssetFilter context="destination" />
+          </Card>
+        </div>
+      </div>
+
+      {/* Right Content */}
+      <div className={`flex flex-1 flex-col p-6`}>
+        <div className="mb-8 flex justify-between">
+          <p>nav</p>
+        </div>
+
+        <div className="flex flex-1 flex-col items-center justify-center">
+          <div className="w-full max-w-md">
+            <Widget
+              disableShadowDom
+              filter={{
+                source: {
+                  "cosmoshub-4": ["uatom"],
+                },
+              }}
+            />
           </div>
+        </div>
 
-          {/* Right Content */}
-          <div className={`flex flex-1 flex-col p-6`}>
-            <div className="mb-8 flex justify-between">
-              <p>nav</p>
-            </div>
+        <div className="mt-4 flex justify-end">
+          <div className="flex items-center gap-2 text-sm text-zinc-400">
+            <span>Background</span>
 
-            <div className="flex flex-1 flex-col items-center justify-center">
-              <div className="w-full max-w-md">
-                <Widget
-                  disableShadowDom
-                  filter={{
-                    source: {
-                      "cosmoshub-4": ["uatom"],
-                    },
-                  }}
-                />
-              </div>
-            </div>
-
-            <div className="mt-4 flex justify-end">
-              <div className="flex items-center gap-2 text-sm text-zinc-400">
-                <span>Background</span>
-
-                <div className="h-6 w-6 cursor-pointer overflow-hidden rounded-full border border-zinc-600">
-                  <input
-                    type="color"
-                    value={backgroundColor}
-                    onChange={(e) => saveBackgroundColor(e.target.value)}
-                    className="cursor-pointer border-none opacity-0 outline-none"
-                    style={{
-                      backgroundColor: backgroundColor,
-                    }}
-                  />
-                </div>
-              </div>
+            <div className="h-6 w-6 cursor-pointer overflow-hidden rounded-full border border-zinc-600">
+              <input
+                type="color"
+                value={backgroundColor}
+                onChange={(e) => saveBackgroundColor(e.target.value)}
+                className="cursor-pointer border-none opacity-0 outline-none"
+                style={{
+                  backgroundColor: backgroundColor,
+                }}
+              />
             </div>
           </div>
         </div>
