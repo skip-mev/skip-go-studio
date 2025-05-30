a43597c61ba9686971a1ad51096ebaba9ca2dbdc
Todd Kao <todd.kakao@gmail.com>
2025-05-30 13:06:50 -0400

    Update Checkbox.tsx

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,11 +2,20 @@ import { cn } from "@/utils/ui";
 
 export const Checkbox = ({ checked }: { checked?: boolean }) => {
   return (
-    <div
-      className={cn(
-        "w-6 h-6 rounded-full group-hover:border-[#FFFFFF] border-2 border-[#3A3A3A]",
-        checked ? "bg-[#FFFFFF]" : "bg-[#3A3A3A]"
-      )}
-    />
+    <div className="inline-flex items-center">
+      <div
+        className={cn(
+          "w-8 h-5 flex items-center rounded-full px-0.5 transition-colors duration-300",
+          checked ? "bg-green-500" : "bg-red-500"
+        )}
+      >
+        <div
+          className={cn(
+            "w-4 h-4 bg-white rounded-full shadow-sm transform transition-transform duration-300",
+            checked ? "translate-x-[12px]" : "translate-x-0"
+          )}
+        />
+      </div>
+    </div>
   );
 };
