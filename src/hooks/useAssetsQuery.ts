ee9cd261db47121ab4631221befded400d0fd048
Nur Fikri <codingki@gmail.com>
2025-07-29 03:32:39 +0700

    add fetch retries mechanism

--- src/hooks/useAssetsQuery.ts
+++ src/hooks/useAssetsQuery.ts
@@ -1,22 +1,37 @@
 import { assets } from "@skip-go/client";
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
-export const useAssetsQuery = (props?: Omit<UseQueryOptions<Awaited<ReturnType <typeof assets>>>, "queryKey" | "queryFn">) => {
-
+export const useAssetsQuery = (
+  props?: Omit<
+    UseQueryOptions<Awaited<ReturnType<typeof assets>>>,
+    "queryKey" | "queryFn"
+  >
+) => {
   return useQuery({
     queryKey: ["assets"],
     queryFn: async () => {
-       await new Promise((resolve) => setTimeout(resolve, 1500));
-      return await assets({
-        includeCw20Assets: true,
-        includeEvmAssets: true,
-        includeSvmAssets: true,
-        abortDuplicateRequests: false,
-      });
+        for (let attempt = 1; attempt <= 3; attempt++) {
+          const result = await assets({
+            includeCw20Assets: true,
+            includeEvmAssets: true,
+            includeSvmAssets: true,
+            abortDuplicateRequests: false,
+          });
+          console.log("Attempting to fetch assets, attempt number:", attempt);
+          if (result !== undefined) {
+            return result;
+          }
+
+          if (attempt < 3) {
+            await new Promise((resolve) => setTimeout(resolve, 500));
+          }
+        }
+        throw new Error("Failed to fetch assets after maximum retries.");
     },
+    retry: 3,
     // select: (data) => {
     //   return data[id];
     // },
     ...props,
   });
-}
+};
--- src/hooks/useChainsQuery.ts
+++ src/hooks/useChainsQuery.ts
@@ -1,19 +1,33 @@
-
 import { chains } from "@skip-go/client";
 import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 
-export const useChainsQuery = (props?: Omit<UseQueryOptions<Awaited<ReturnType <typeof chains>>>, "queryKey" | "queryFn">) => {
+export const useChainsQuery = (
+  props?: Omit<
+    UseQueryOptions<Awaited<ReturnType<typeof chains>>>,
+    "queryKey" | "queryFn"
+  >
+) => {
   return useQuery({
     queryKey: ["chains"],
     queryFn: async () => {
-      // sleep 1s
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      return await chains({
-        includeEvm: true,
-        includeSvm: true,
-        abortDuplicateRequests: false,
-      });
+      for (let attempt = 1; attempt <= 3; attempt++) {
+        const result = await chains({
+          includeEvm: true,
+          includeSvm: true,
+          abortDuplicateRequests: false,
+        });
+
+        if (result !== undefined) {
+          return result;
+        }
+
+        if (attempt < 3) {
+          await new Promise((resolve) => setTimeout(resolve, 500));
+        }
+      }
+      throw new Error("Failed to fetch chains after maximum retries.");
     },
+    retry: 3,
     ...props,
   });
-}
+};
